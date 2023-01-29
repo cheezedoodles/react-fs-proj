ed528712a91f043639f57fc9b37c7a4a27cc1ac4
cheezedoodles <hougvapes@gmail.com>
2023-01-29 21:38:41 +0300

    changed Author component to load data from the api

--- src/frontend/components/pages/AuthorsIndex.js
+++ src/frontend/components/pages/AuthorsIndex.js
@@ -3,7 +3,7 @@ import htm from 'htm'
 import { Link } from 'react-router-dom'
 import superagent from 'superagent'
 import { Header } from '../Header.js'
-import { authors } from '../../../data/authors.js'
+
 
 const html = htm.bind(react.createElement)
 
