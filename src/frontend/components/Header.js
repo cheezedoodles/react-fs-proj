0ca66a3c9f98f7b8622fef4f58ac9badd8f53048
cheezedoodles <hougvapes@gmail.com>
2023-01-29 19:19:26 +0300

    created Header component

---
+++ src/frontend/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import htm from 'htm';
+import { Link } from 'react-router-dom';
+
+const html = htm.bind(React.createElement)
+
+export class Header extends React.Component {
+  render() {
+    return html`
+    <header>
+      <h1>
+        <${Link} to="/">My library</>
+      </h1>
+    </header>
+    `
+  }
+}
